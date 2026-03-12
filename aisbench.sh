14575947546d1041fdd5713e25d82816bd8737e0
xy3.peng@icloud.com <pxy@pxydeMac-mini.local>
2026-03-12 20:25:03 +0800

    fix

--- aisbench.sh
+++ aisbench.sh
@@ -1,9 +1,8 @@
-
 TASK_NAME="${1:?Usage: $0 <TASK_NAME>}"
 BATCH_SIZE="${2:?Usage: $0 <batch_size>}"
 LOG_DIR="/root/autodl-tmp/pxy/share/workspace/logs/${TASK_NAME}"
 
-source /root/autodl-tmp/py_venv/tester/bin/activate
+source /root/autodl-tmp/py_venv/xy_tester/bin/activate
 cd /root/autodl-tmp/pxy/share/workspace/benchmark
 
 sed -i "s/batch_size=[0-9]*/batch_size=$BATCH_SIZE/g" ais_bench/benchmark/configs/models/vllm_api/vllm_api_stream_chat_multiturn.py
