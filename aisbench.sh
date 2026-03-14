262026310c6e00028cd9ca18f33699f559dfaebe
xy3pe <xy3.peng@icloud.com>
2026-03-14 18:35:15 +0800

    update

--- aisbench.sh
+++ aisbench.sh
@@ -6,4 +6,7 @@ source /root/autodl-tmp/py_venv/xy_tester/bin/activate
 cd /root/autodl-tmp/pxy/share/workspace/benchmark
 
 sed -i "s/batch_size=[0-9]*/batch_size=$BATCH_SIZE/g" ais_bench/benchmark/configs/models/vllm_api/vllm_api_stream_chat_multiturn.py
-ais_bench --models vllm_api_stream_chat_multiturn --datasets sharegpt_gen --mode perf --num-warmups 0 > $LOG_DIR/aisbench_${TASK_NAME}_bs${BATCH_SIZE}.log
+
+# 不断轮询压测模式 --pressure --pressure-time 30 
+# --work-dir 或 -w 指定评测工作目录，用于保存输出结果。默认 outputs/default。
+ais_bench --models vllm_api_stream_chat_multiturn --datasets sharegpt_gen --mode perf --num-warmups 0 --work-dir ${LOG_DIR}/output_${TASK_NAME}_bs${BATCH_SIZE} > $LOG_DIR/aisbench_${TASK_NAME}_bs${BATCH_SIZE}.log
